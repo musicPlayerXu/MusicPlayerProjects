668f7af11ccec31f9b18a2729080746a0f16c6bc
周标 <2280379372@qq.com>
2015-06-30 19:05:53 +0800

    github Test1

---
+++ MusicPlayer/MusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by administrator on 15/6/30.
+//  Copyright (c) 2015年 gem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicPlayer/MusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicPlayer
+//
+//  Created by administrator on 15/6/30.
+//  Copyright (c) 2015年 gem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
